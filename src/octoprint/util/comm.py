d8767ed8beebab9c0dca5f192f4a81def6203079
fflosi <fernando.flosi@hotmail.com>
2022-01-12 23:55:33 -0300

    Update comm.py

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -3356,7 +3356,11 @@ class MachineCom(object):
 
     def _continue_sending(self):
         # Ensure we have at least one line in the send queue, but don't spam it
-        while self._active and not self._send_queue.qsize():
+        # while self._active and not self._send_queue.qsize():
+        while self._active and (self._send_queue.qsize() < self._ack_max):
+            self._logger.debug(
+                "self._send_queue.qsize() = " + self._send_queue.qsize()
+            )
             job_active = self._state in (
                 self.STATE_STARTING,
                 self.STATE_PRINTING,
